a1b08a0926ac81cc5e808b758247eb100db76438
Anders Emil Nielsen <aemilnielsen@gmail.com>
2016-02-11 16:09:53 +0100

    Extract all key definition

--- smache/dependency_graph/redis_dependency_graph.py
+++ smache/dependency_graph/redis_dependency_graph.py
@@ -1,4 +1,6 @@
 class RedisDependencyGraph:
+    all_key = 'all'
+
     def __init__(self, redis_con):
         self.redis_con = redis_con
 
@@ -7,11 +9,11 @@ class RedisDependencyGraph:
         self.redis_con.sadd(key, dep_key)
 
     def add_data_source_dependency(self, data_source_id, dep_key):
-        self.add_dependency(data_source_id, 'all', dep_key)
+        self.add_dependency(data_source_id, self.all_key, dep_key)
 
     def values_depending_on(self, data_source_id, entity_id):
         entity_key = self._entity_key(data_source_id, entity_id)
-        all_key = self._entity_key(data_source_id, 'all')
+        all_key = self._entity_key(data_source_id, self.all_key)
         return self.redis_con.sunion(entity_key, all_key)
 
     def _entity_key(self, data_source_id, entity_id):
