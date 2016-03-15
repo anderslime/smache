3a56064e430085d9a4c6ce7c0953256d74cd5c97
Anders Emil Nielsen <aemilnielsen@gmail.com>
2016-03-15 12:29:49 +0100

    Setup benchmarking

--- tests/benchmarks/test_bm_performance.py
+++ tests/benchmarks/test_bm_performance.py
@@ -1,28 +1,66 @@
-import time, pytest
+import redis
+from rq import Queue
+from smache import Smache, MongoDataSource
+from tests.mongo_helper import User, Handin, db
 
-class MyBenchmark:
-    def __init__(self):
-        self.benchmarks = []
-        self.append_stats = self.benchmarks.append
+import logging, sys
 
-mybenchmark = MyBenchmark()
 
-@pytest.fixture(scope="function")
-def bm(benchmark):
-    benchmark._add_stats = mybenchmark.append_stats
-    return benchmark
+# Setup
+redis_con = redis.StrictRedis(host='localhost', port=6379, db=0)
+worker_queue = Queue('test_queue', connection=redis_con)
+smache = Smache(worker_queue=worker_queue, write_through=True)
 
-@pytest.fixture(scope="function")
-def bmstats():
-    return mybenchmark
+user   = MongoDataSource(User)
+handin = MongoDataSource(Handin)
 
-def hello():
-    time.sleep(0.01)
+smache.add_sources(user, handin)
 
-def test_performance(bm, bmstats):
-    bm.pedantic(hello, args=(), iterations=1, rounds=10)
+@smache.computed(user, relations=[(handin, lambda handin: handin.users)])
+def score(user):
+    handins = Handin.objects(users=user)
+    total_score = sum(handin.score for handin in handins)
+    avg_score = total_score / len(handins)
+    return {
+        'name': user.name,
+        'age': user.age,
+        'total_score': total_score,
+        'avg_score': avg_score
+    }
 
-    for bench in bmstats.benchmarks:
-        print bench.stats.mean
+num_of_handins_per_user = 100
 
+def build_setup(num_of_users):
+    def data_setup():
+        db.drop_database('testdb')
+
+        users = [User(name='Anders', age=25) for _ in range(num_of_users)]
+        User.objects.insert(users)
+        saved_users = User.objects()
+
+        handins = [Handin(users=[user], score=10) for user in saved_users for _ in range(num_of_handins_per_user)]
+        Handin.objects.insert(handins)
+
+        for user in saved_users:
+            score(user)
+
+    return data_setup
+
+def make_underlying_data_change():
+    print "USER"
+    user = User.objects.first()
+    user.save()
+    logger.info("HELLO INFO")
+    logger.debug("HELLO INFO")
+    print "USER SAVED"
+
+
+def run_benchmark(benchmark, num_of_users):
+    benchmark.pedantic(make_underlying_data_change,
+                        setup=build_setup(num_of_users),
+                        iterations=1,
+                        rounds=1)
+
+def test_performance_10_users(benchmark):
+    run_benchmark(benchmark, 1)
     assert False
--- tests/mongo_helper.py
+++ tests/mongo_helper.py
@@ -1,7 +1,11 @@
-from mongoengine import Document, StringField, IntField, connect
+from mongoengine import Document, StringField, IntField, connect, ListField, ReferenceField
 
 db = connect('testdb', host='localhost', port=27017,)
 
 class User(Document):
     name = StringField()
     age = IntField()
+
+class Handin(Document):
+    users = ListField(ReferenceField(User))
+    score = IntField()
