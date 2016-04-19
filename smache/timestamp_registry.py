9c8b0de56e0b66e092e1f34a415000428499cdbb
Anders Emil Nielsen <aemilnielsen@gmail.com>
2016-04-19 13:10:04 +0200

    Extend timestamp registry

--- smache/timestamp_registry.py
+++ smache/timestamp_registry.py
@@ -2,8 +2,18 @@ class TimestampRegistry:
     def __init__(self, redis_con):
         self._redis_con = redis_con
 
-    def next_timestamp(self, key):
-        return self._redis_con.incr(self._timestamp_key(key))
+    def state_timestamp(self, key):
+        return self._redis_con.get(self._state_ts_key(key))
 
-    def _timestamp_key(self, key):
-        return 'smache:timestamp:{}'.format(key)
+    def value_timestamp(self, key):
+        return self._redis_con.get(self._value_ts_key(key))
+
+    def next_timestamp(self, key):
+        redis_key = 'smache:timestamp:next:{}'.format(key)
+        return self._redis_con.incr(redis_key)
+
+    def _state_ts_key(self, key):
+        return 'smache:timestamp:state:{}'.format(key)
+
+    def _value_ts_key(self, key):
+        return 'smache:timestamp:value:{}'.format(key)
