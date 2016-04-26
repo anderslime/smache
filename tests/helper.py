dd039492811bf471997dac66293238541c550dbb
Anders Emil Nielsen <aemilnielsen@gmail.com>
2016-04-26 10:18:32 +0200

    Do not use default redis db for tests

--- tests/helper.py
+++ tests/helper.py
@@ -2,7 +2,7 @@ from rq import SimpleWorker
 from smache.data_sources.in_memory_data_source import InMemoryEntity
 import redis
 
-redis_con = redis.StrictRedis(host='localhost', port=6379, db=0)
+redis_con = redis.StrictRedis(host='localhost', port=6379, db=5)
 
 
 class DummyA(InMemoryEntity):
