1c6d80d929127b476301271166182ba0f08fd95a
Anders Emil Nielsen <aemilnielsen@gmail.com>
2016-06-07 12:55:46 +0200

    Fixed linting

--- tests/smache/test_options.py
+++ tests/smache/test_options.py
@@ -5,6 +5,7 @@ from mock import Mock
 from smache.options import Options
 from smache.schedulers import AsyncScheduler
 
+
 def test_defaults():
     options = Options()
 
@@ -14,19 +15,24 @@ def test_defaults():
     assert isinstance(options.worker_queue, Queue)
     assert isinstance(options.scheduler, AsyncScheduler)
 
+
 def test_write_through_is_set():
     assert Options(write_through=False).write_through == False
 
+
 def test_debug_is_set():
     assert Options(debug=True).debug == True
+
 
 def test_redis_con_is_set():
     redis_con = StrictRedis()
     assert Options(redis_con=redis_con).redis_con == redis_con
 
+
 def test_worker_queue_is_set():
     worker_queue = Mock()
     assert Options(worker_queue=worker_queue).worker_queue == worker_queue
+
 
 def test_scheduler_is_set():
     scheduler = Mock()
